a841c927eb59f6781876d9d61341e990ff1d192b
mdekauwe <mdekauwe@gmail.com>
2018-05-31 16:05:59 +1000

    Added histogram

--- plot_histogram.py
+++ plot_histogram.py
@@ -91,7 +91,7 @@ def main():
     ax2.tick_params(direction='in', length=4)
 
     ax1.set_ylabel("Density")
-    ax1.set_xlabel("Foley's Index", position=(0.5, 1.0))
+    ax1.set_xlabel("Foley's Index", position=(0.5, 1.5))
 
     plt.setp(ax2.get_yticklabels(), visible=False)
 

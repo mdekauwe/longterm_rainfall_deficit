2070ffc335ebf1dfccea7a7ef069483c1a28648d
mdekauwe <mdekauwe@gmail.com>
2018-05-31 15:49:19 +1000

    Added cont scatter

--- plot_drought_index.py
+++ plot_drought_index.py
@@ -36,15 +36,27 @@ def main():
     fname = os.path.join(odir, "map.bin")
     map = np.fromfile(fname, dtype=np.float32).reshape(nrows, ncols)
 
-    plt.plot(map, dmin, "k.", alpha=0.1)
-    plt.xlim(0, 2500)
+    #plt.plot(map, dmin, "k.", alpha=0.1)
+    #plt.xlim(0, 2500)
+    #plt.show()
+    aus = dmin[200:280,220:320]
+    aus_map = map[200:280,220:320]
+    afr1 = dmin[100:260,0:120]
+    afr2 = dmin[100:260,680:720]
+    afr = np.hstack((afr2, afr1))
+    afr1_map = map[100:260,0:120]
+    afr2_map = map[100:260,680:720]
+    afr_map = np.hstack((afr2_map, afr1_map))
+
+    plt.plot(afr_map, afr, "k.", alpha=0.1)
+    plt.plot(aus_map, aus, "r.", alpha=0.1)
+    #plt.xlim(0, 2500)
     plt.show()
 
-
-    dmin = np.where(dmin > -1.0, np.nan, dmin)
-    plt.imshow(dmin, origin="upper")
-    plt.colorbar()
-    plt.show()
+    #dmin = np.where(dmin > -1.0, np.nan, dmin)
+    #plt.imshow(dmin, origin="upper")
+    #plt.colorbar()
+    #plt.show()
 
 if __name__ == "__main__":
 
